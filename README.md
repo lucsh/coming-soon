1fd36fa8cd039bbe2d0375507c8b33d10663836b
Lucas Del Pozzi <me@luc.sh>
2020-01-21 20:12:58 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Inlcudes
+
+-   Simple Header / Content / Footer Flexbox
+-   Coming Soon Template
+
+## Scripts
+
+-   "build"
+-   "build:prod"
+-   "dev" _Creates an http server @ http://localhost:2000_
---
+++ src/app.js
@@ -0,0 +1,13 @@
+import './assets/scss/app.scss';
+/**
+ * Remember to import assets fow webpack
+ */
+
+// import './assets/media/logo/logo.svg';
+
+
+import './assets/media/SVG/construccion.svg';
+import './assets/media/SVG/location_icon.svg';
+import './assets/media/SVG/logos.svg';
+import './assets/media/SVG/ig_icon.svg';
+import './assets/media/SVG/fb_icon.svg';
